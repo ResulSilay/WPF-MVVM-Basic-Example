88118dd11cf0a38f1d3687ed7a6d831e6bcd5707
Resul Silay <resulsilay@gmail.com>
2020-02-02 20:57:05 +0300

    First commit.

---
+++ Notebook/UI/Model/Note.cs
@@ -0,0 +1,73 @@
+﻿using Notebook.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Notebook.UI.Model
+{
+    public class Note : ObservableBase
+    {
+        private string title;
+        private string description;
+        private DateTime datetime = DateTime.Now;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    RaisePropertyChanged("Description");
+                }
+            }
+        }
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return datetime;
+            }
+            set
+            {
+                if (datetime != value)
+                {
+                    datetime = value;
+                    RaisePropertyChanged("DateTime");
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            Title = null;
+            Description = null;
+            DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ Notebook/UI/ViewModel/NoteViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Notebook.Base;
+using Notebook.UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Notebook.UI.ViewModel
+{
+    public class NoteViewModel : ViewModelBase
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+        public ICommand OnNoteDetailCommand { get; set; }
+        public ICommand OnNoteInsertCommand { get; set; }
+        public ICommand OnNoteDeleteCommand { get; set; }
+        public ICommand OnExitCommand { get; set; }
+
+        private Note noteModel;
+        public Note NoteModel
+        {
+            get { return noteModel; }
+            set { noteModel = value; }
+        }
+
+        public NoteViewModel()
+        {
+            noteModel = new Note();
+            Load();
+
+            OnNoteDetailCommand = new RelayCommand<Note>(OnDetail);
+            OnNoteInsertCommand = new RelayCommand<Note>(OnInsert);
+            OnNoteDeleteCommand = new RelayCommand<Note>(OnDelete);
+            OnExitCommand = new RelayCommand<Note>(OnExit);
+        }
+
+        public void Load()
+        {
+            Notes = new ObservableCollection<Note>
+            {
+                new Note { Title = "Merhaba Dünya", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Hafta sonu", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "MVVM giriş. ", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Merhaba Dünya", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Hafta sonu", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "MVVM giriş. ", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Merhaba Dünya", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Hafta sonu", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "MVVM giriş. ", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Merhaba Dünya", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "Hafta sonu", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now },
+                new Note { Title = "MVVM giriş. ", Description = "Bu bir WPF uygulamasıdır. Basit ve minimalist bir not defteri.", DateTime = DateTime.Now }
+            };
+        }
+
+        public void OnDetail(Note note)
+        {
+            if (note != null)
+            {
+                MessageBox.Show(String.Format("{0}\n{1}\n\n{2}", note.Title, note.Description, note.DateTime.ToString()));
+            }
+        }
+
+        public void OnInsert(Note note)
+        {
+
+            if (note != null && note.Title != null && note.Description != null)
+            {
+                Notes.Add(new Note { Title = note.Title, Description = note.Description, DateTime = note.DateTime });
+                NoteModel.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurunuz.");
+            }
+        }
+
+        public void OnDelete(Note note)
+        {
+            if (note != null)
+            {
+                Notes.Remove(note);
+            }
+        }
+
+        public void OnExit(Object item) => Application.Current.Shutdown();
+    }
+}
